8f67cbbc61a0cc058003a25cad287b0703208e70
Marat Radchenko <marat@slonopotamus.org>
2021-09-28 22:42:12 +0300

    resolves #4 add automated publishing to chocolatey.org

---
+++ choco-publish.ps1
@@ -0,0 +1,12 @@
+$ErrorActionPreference = 'Stop'
+
+New-Item -ItemType Directory -Force -Path target/choco/tools | Out-Null
+
+$version = $args[0] -replace '.*/', "$1"
+$msi_hash = (Get-FileHash "target/wix/stevedore-$version-x86_64.msi").Hash
+
+(Get-Content choco/stevedore.nuspec) -replace '{{ version }}', $version | Set-Content "target/choco/stevedore-$version.nuspec"
+(Get-Content choco/tools/chocolateyinstall.ps1) -replace '{{ version }}', $version -replace '{{ sha256 }}', $msi_hash | Set-Content target/choco/tools/chocolateyinstall.ps1
+choco pack -out=target/choco/ "target/choco/stevedore-$version.nuspec"
+choco apikey --key $argv[1]
+choco push "target/choco/stevedore.$version.nupkg"
---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/slonopotamus/stevedore/releases/download/{{ version }}/stevedore-{{ version }}-x86_64.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url64bit      = $url64
+
+  softwareName  = 'Stevedore'
+
+  checksum64    = '{{ sha256 }}'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
