d27a526eb6128591dd7be6131f974ce6f6d6dfb7
Marat Radchenko <marat@slonopotamus.org>
2021-09-28 22:44:26 +0300

    fix chocolatey apikey host

--- choco-publish.ps1
+++ choco-publish.ps1
@@ -8,5 +8,5 @@ $msi_hash = (Get-FileHash "target/wix/stevedore-$version-x86_64.msi").Hash
 (Get-Content choco/stevedore.nuspec) -replace '{{ version }}', $version | Set-Content "target/choco/stevedore-$version.nuspec"
 (Get-Content choco/tools/chocolateyinstall.ps1) -replace '{{ version }}', $version -replace '{{ sha256 }}', $msi_hash | Set-Content target/choco/tools/chocolateyinstall.ps1
 choco pack -out=target/choco/ "target/choco/stevedore-$version.nuspec"
-choco apikey --key $args[1]
+choco apikey --key $args[1] -source https://push.chocolatey.org/
 choco push "target/choco/stevedore.$version.nupkg"
