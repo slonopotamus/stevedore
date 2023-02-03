72db667aba32a15aa95f68bb8b35db260c22860b
Marat Radchenko <marat@slonopotamus.org>
2023-02-03 02:31:49 -0800

    Drop Linux containers support, update to Docker 23.0.0 (#59)

--- build.rs
+++ build.rs
@@ -1,51 +1,26 @@
 use std::env;
 use std::fs::{create_dir_all, File};
 use std::io;
-use std::io::{Read, Write};
+use std::io::Write;
 use std::path::{Path, PathBuf};
-use std::process::Command;
 
 use const_format::formatcp;
-use flate2::write::GzEncoder;
-use flate2::Compression;
 use sha2::{Digest, Sha256};
 use zip::ZipArchive;
 
-const DOCKER_VERSION: &str = "20.10.21";
+const DOCKER_VERSION: &str = "23.0.0";
 
 const DOCKER_WIN64_URL: &str =
     formatcp!("https://download.docker.com/win/static/stable/x86_64/docker-{DOCKER_VERSION}.zip");
-const DOCKER_WIN64_SHA: &str = "e5c9e9b965d72725b4280bba94e497e9789c84609ed646f0193018d9b3598306";
+const DOCKER_WIN64_SHA: &str = "7cdf8dfb19b545079bee399b927b77220987b0428501e0d1525311d31ca8b29e";
 
-const DOCKER_LINUX_URL: &str =
-    formatcp!("https://download.docker.com/linux/static/stable/x86_64/docker-{DOCKER_VERSION}.tgz");
-const DOCKER_LINUX_SHA: &str = "2582bed8772b283bda9d4565c0af76ee653c93d93dc6b8d0aad795d731a1bb81";
-
-const SHMOBY_VERSION: &str = formatcp!("{DOCKER_VERSION}.1");
-const SHMOBY_URL: &str = formatcp!(
-    "https://github.com/slonopotamus/shmoby/releases/download/v{SHMOBY_VERSION}/dockerd.exe"
-);
-const SHMOBY_SHA: &str = "3d75ff38f82cb5f83d66f7eb9c059a0c28b2b006de1529979f542d328c420750";
-
-const ALPINE_URL: &str = "https://dl-cdn.alpinelinux.org/alpine/v3.15/releases/x86_64/alpine-minirootfs-3.15.6-x86_64.tar.gz";
-const ALPINE_SHA: &str = "1ae0c0e09a7a5dc6f1550c25c8793124c2ae52825cbf035a7ad249d628953cec";
-
-const DOCKER_COMPOSE_VERSION: &str = "2.12.2";
+const DOCKER_COMPOSE_VERSION: &str = "2.15.1";
 const DOCKER_COMPOSE_URL: &str = formatcp!("https://github.com/docker/compose/releases/download/v{DOCKER_COMPOSE_VERSION}/docker-compose-windows-x86_64.exe");
-const DOCKER_COMPOSE_SHA: &str = "0356b20170258be37295774b89a32b294dd524b646ec6df896026a82c43d6f62";
+const DOCKER_COMPOSE_SHA: &str = "538598025dc9ae41298abf859d5b312fba40359c691d9b14527375f4633cbe65";
 
-const DOCKER_SCAN_VERSION: &str = "0.21.0";
+const DOCKER_SCAN_VERSION: &str = "0.23.0";
 const DOCKER_SCAN_URL: &str = formatcp!("https://github.com/docker/scan-cli-plugin/releases/download/v{DOCKER_SCAN_VERSION}/docker-scan_windows_amd64.exe");
-const DOCKER_SCAN_SHA: &str = "a8171f853aa3708cd7decec0c181216795b0ec86aa859edd76f381a0c77978b4";
-
-const DOCKER_WSL_PROXY_VERSION: &str = "0.0.7";
-const DOCKER_WSL_PROXY_URL: &str = formatcp!("https://github.com/slonopotamus/docker-wsl-proxy/releases/download/{DOCKER_WSL_PROXY_VERSION}/docker-wsl-proxy.exe");
-const DOCKER_WSL_PROXY_SHA: &str =
-    "ddb2097ae755cd0f7d266a713fc71ff3e9cf32ecf60e1a67dec726b540b86b86";
-
-const KUBECTL_VERSION: &str = "1.25.3";
-const KUBECTL_URL: &str = formatcp!("https://storage.googleapis.com/kubernetes-release/release/v{KUBECTL_VERSION}/bin/windows/amd64/kubectl.exe");
-const KUBECTL_SHA: &str = "ff78c3cb3cba7ce397db01ca4ac4769c476efbbccbd5ff007a7d4b976a6e4ac1";
+const DOCKER_SCAN_SHA: &str = "cd3ddee1deb75585619601e40f737544a81555a5cd6153a90e58d1cd30e9a669";
 
 const WINCRED_VERSION: &str = "0.7.0";
 const WINCRED_URL: &str = formatcp!("https://github.com/docker/docker-credential-helpers/releases/download/v{WINCRED_VERSION}/docker-credential-wincred-v{WINCRED_VERSION}.windows-amd64.exe");
@@ -92,11 +67,6 @@ fn build_wincred(dest_dir: &Path) {
     download_file(WINCRED_URL, WINCRED_SHA, &dest_path);
 }
 
-fn build_shmoby(dest_dir: &Path) {
-    let dest_path = dest_dir.join("dockerd.exe");
-    download_file(SHMOBY_URL, SHMOBY_SHA, &dest_path);
-}
-
 fn download_file(uri: &str, expected_sha: &str, dest: &Path) {
     if let Ok(mut file) = File::open(dest) {
         let mut digest = Sha256::new();
@@ -127,168 +97,13 @@ fn build_docker_scan_plugin(dest_dir: &Path) {
     download_file(DOCKER_SCAN_URL, DOCKER_SCAN_SHA, &dest_path);
 }
 
-fn build_docker_wsl_proxy(dest_dir: &Path) {
-    let dest_path = dest_dir.join("docker-wsl-proxy.exe");
-    download_file(DOCKER_WSL_PROXY_URL, DOCKER_WSL_PROXY_SHA, &dest_path);
-}
-
-fn build_kubectl(dest_dir: &Path) {
-    let dest_path = dest_dir.join("kubectl.exe");
-    download_file(KUBECTL_URL, KUBECTL_SHA, &dest_path);
-}
-
-fn run_cmd(cmd: &mut Command) -> String {
-    let output = cmd.output().unwrap();
-    assert!(
-        output.status.success(),
-        "{:?} returned {:?}\nSTDOUT:{}\nSTDERR:\n{}",
-        cmd,
-        output.status,
-        std::str::from_utf8(&output.stdout).unwrap(),
-        std::str::from_utf8(&output.stderr).unwrap(),
-    );
-    String::from(std::str::from_utf8(&output.stdout).unwrap().trim())
-}
-
-/**
-1. You can't use Docker in this function because GitHub Actions doesn't support nested virtualization
-2. You can't use WSL2 in this function because Windows Server only has WSL1
- */
-fn build_wsl_tarball(dest_dir: &Path) {
-    const DISTRIBUTION_NAME: &str = "stevedore";
-    const STAGING_DISTRIBUTION_NAME: &str = formatcp!("{DISTRIBUTION_NAME}-staging");
-
-    // Download Alpine Linux rootfs
-    let alpine_tgz = dest_dir.join(formatcp!("alpine.tar.gz"));
-    download_file(ALPINE_URL, ALPINE_SHA, &alpine_tgz);
-
-    // Download Linux Docker binaries
-    let docker_tgz = dest_dir.join("docker.tar.gz");
-    download_file(DOCKER_LINUX_URL, DOCKER_LINUX_SHA, &docker_tgz);
-
-    // Remove WSL distribution in case it already exists
-    let _ = Command::new("wsl")
-        .arg("--unregister")
-        .arg(STAGING_DISTRIBUTION_NAME)
-        .output();
-
-    // Import Alpine Linux rootfs into WSL
-    run_cmd(
-        Command::new("wsl")
-            .arg("--import")
-            .arg(STAGING_DISTRIBUTION_NAME)
-            .arg(dest_dir)
-            .arg(alpine_tgz),
-    );
-
-    // Add docker into WSL
-    // TODO(https://github.com/slonopotamus/stevedore/issues/26): Instead, use Docker binaries we just downloaded
-    run_cmd(
-        Command::new("wsl")
-            .arg("--distribution")
-            .arg(STAGING_DISTRIBUTION_NAME)
-            .arg("--")
-            .arg("apk")
-            .arg("add")
-            .arg("--no-cache")
-            .arg("docker")
-            .arg("socat"),
-    );
-
-    let uncompressed_tarball_path = dest_dir.join(formatcp!("{DISTRIBUTION_NAME}.tar"));
-    let compressed_tarball_path = dest_dir.join(formatcp!("{DISTRIBUTION_NAME}.tar.gz"));
-
-    // Export tarball from WSL
-    run_cmd(
-        Command::new("wsl")
-            .arg("--export")
-            .arg(STAGING_DISTRIBUTION_NAME)
-            .arg(&uncompressed_tarball_path),
-    );
-
-    // Compress tarball
-    // TODO: pipe directly from wsl --export
-    {
-        let mut uncompressed_tarball = File::open(uncompressed_tarball_path).unwrap();
-        let compressed_tarball = File::create(compressed_tarball_path).unwrap();
-        let mut encoder = GzEncoder::new(compressed_tarball, Compression::default());
-        std::io::copy(&mut uncompressed_tarball, &mut encoder).unwrap();
-    }
-
-    // Cleanup WSL
-    run_cmd(
-        Command::new("wsl")
-            .arg("--unregister")
-            .arg(STAGING_DISTRIBUTION_NAME),
-    );
-}
-
-fn copy_redist_msm(dest_dir: &Path) {
-    let tool = cc::windows_registry::find_tool("x86_64-msvc", "cl.exe").unwrap();
-
-    let vc_dir = tool
-        .path()
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap();
-
-    let version_path = vc_dir
-        .join("Auxiliary")
-        .join("Build")
-        .join("Microsoft.VCRedistVersion.default.txt");
-
-    let mut version_file = File::open(version_path).unwrap();
-    let mut version = String::new();
-    version_file.read_to_string(&mut version).unwrap();
-    let version = version.trim();
-
-    let msm_dir = vc_dir
-        .join("Redist")
-        .join("MSVC")
-        .join(version)
-        .join("MergeModules");
-
-    let msm_suffix = "CRT_x64.msm";
-
-    for f in msm_dir.read_dir().unwrap() {
-        let f = f.unwrap();
-        if f.file_name().to_string_lossy().ends_with(msm_suffix) {
-            std::fs::copy(f.path(), dest_dir.join("vcredist.msm")).unwrap();
-            return;
-        }
-    }
-
-    panic!("Failed to find '*{msm_suffix}' {msm_dir:?}");
-}
-
 fn main() {
     let dest_dir = get_dest_dir();
 
-    copy_redist_msm(&dest_dir);
-    build_wsl_tarball(&dest_dir);
     build_docker(&dest_dir);
-    build_shmoby(&dest_dir);
     build_docker_compose(&dest_dir);
     build_docker_scan_plugin(&dest_dir);
     build_wincred(&dest_dir);
-    build_docker_wsl_proxy(&dest_dir);
-    build_kubectl(&dest_dir);
 
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("resources/stevedore.ico");
-    res.compile().unwrap();
-
-    println!("cargo:rerun-if-changed=resources/stevedore.ico");
     println!("cargo:rerun-if-changed=build.rs");
 }
